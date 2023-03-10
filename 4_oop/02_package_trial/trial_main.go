8ea19470d3ff74150c967b4db7513d9a4917636c
DiuDiuXiong <WujiZhu1999@canva.com>
2023-03-10 19:25:16 +1100

    next push

--- 4_oop/02_package_trial/trial_main.go
+++ 4_oop/02_package_trial/trial_main.go
@@ -1,17 +1,12 @@
 package main
 
-import (
-	tree "Go/4_oop"
-	mainn "Go/4_oop/03_extend_struct_primitive_type"
-	"fmt"
-)
-
 /*
 import (
 	tree "Go/4_oop"
 	"fmt"
 )*/
 
+/*
 func main() {
 	r := tree.TreeNode{}
 	r.CanDefineHere()
@@ -28,5 +23,4 @@ func main() {
 	fmt.Println(q.IsEmpty())
 	fmt.Println(q.Pop())
 	fmt.Println(q.IsEmpty())
-}
-
+}*/
