fff8ba60d2bf3d0c0775277c1874f9f6deb11fe3
DiuDiuXiong <WujiZhu1999@canva.com>
2023-04-13 20:52:41 +1000

    try test again

--- 13_third_party_library/03_json.go
+++ 13_third_party_library/03_json.go
@@ -46,6 +46,11 @@ type OrderSelfDefined struct {
 	TotalPrice float64 `json:"totalPrice"`
 }
 
+type ComposeType struct {
+	MyOrder OrderSelfDefined `json:"item"`
+	ID      string           `json:"id"`
+}
+
 func jsonSelfDefinedName() {
 	o := OrderSelfDefined{
 		ID:         "1234",
@@ -67,6 +72,33 @@ func jsonSelfDefinedName() {
 	fmt.Printf("%s\n", b2)
 }
 
+func checkCompose() {
+	o := OrderSelfDefined{
+		ID:         "1234",
+		Name:       "learn go",
+		Quantity:   3,
+		TotalPrice: 30,
+	}
+
+	o2 := ComposeType{
+		MyOrder: o,
+		ID:      "learn go",
+	}
+
+	b2, _ := json.Marshal(o2)
+	fmt.Printf("%s\n", b2)
+}
+
+func unmarshal() {
+	s := `{"item":{"id":"1234","name":"learn go","quantity":3,"totalPrice":30},"id":"learn go"}`
+	var o ComposeType
+	err := json.Unmarshal([]byte(s), &o)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("%+v\n", o)
+}
+
 /*
 1. json.Marshal(<struct>) to get json as a sequence of bytes
 2. after each struct variable definition, add
@@ -74,9 +106,16 @@ func jsonSelfDefinedName() {
 to define the name you wish that field have when converted to json.
 3. add omitempty within "" so if that variable is not defined (zero value), will not be output when called from json.Marshal
 	<var name> <var type> `json:"<name>,omitempty"`
+4. For compose, its json compose json, even if pointer type, json.Marshal(...) will also output same thing
+5. For compose, also slice of object will present in json as list
+6. For unmarshal
+	- Create a object of same type, which will be used for store unmarshal data form json
+	- err := json.Unmarshal([]byte(<json_string>, &<storageObjectCreated>))
 */
 
 func main() {
 	// jsonPrint()
-	jsonSelfDefinedName()
+	//  jsonSelfDefinedName()
+	// checkCompose()
+	unmarshal()
 }
